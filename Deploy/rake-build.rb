15e0721e7e138a83f6837a2373593ed4858f5ef0
Daniel Wertheim <daniel@wertheim.se>
2011-12-08 17:42:50 +0100

    Simplified CI

--- Deploy/rake-build.rb
+++ Deploy/rake-build.rb
@@ -10,77 +10,81 @@ require 'albacore'
 # Environment vars
 #--------------------------------------
 @env_solutionname = 'PineCone'
-@env_projectname = 'PineCone'
-@env_solutionfolderpath = "../Source/"
+@env_projectnamePineCone = 'PineCone'
+@env_solutionfolderpath = "../Source"
 @env_buildversion = "0.42.0" + (ENV['env_buildnumber'].to_s.empty? ? "" : ".#{ENV['env_buildnumber'].to_s}")
 @env_buildconfigname = ENV['env_buildconfigname'].to_s.empty? ? "Release" : ENV['env_buildconfigname'].to_s
 @env_buildname = "#{@env_solutionname}-v#{@env_buildversion}-#{@env_buildconfigname}"
-@env_buildfolderpath = "#{ENV['env_buildfolderpath']}builds/#{@env_buildname}/"
-@env_binariesfolderpath = "#{@env_buildfolderpath}binaries"
+@env_buildfolderpath = @env_buildname
 #--------------------------------------
 #optional if no remote nuget actions should be performed
 @env_nugetPublishApiKey = ENV['env_nugetPublishApiKey']
 @env_nugetPublishUrl = ENV['env_nugetPublishUrl']
 @env_nugetSourceUrl = ENV['env_nugetSourceUrl']
 #--------------------------------------
+# Reusable vars
+#--------------------------------------
+pineConeOutputPath = "#{@env_buildfolderpath}/#{@env_projectnamePineCone}"
+#--------------------------------------
 # Albacore flow controlling tasks
 #--------------------------------------
-task :ci => [:buildIt, :testIt, :packageIt, :deployIt]
+task :ci => [:buildIt, :copyIt, :testIt, :zipIt, :packIt, :publishIt]
 
-task :local => [:buildIt, :testIt, :packageIt]
+task :local => [:buildIt, :copyIt, :testIt, :zipIt, :packIt]
 #--------------------------------------
-task :buildIt => [:versionIt, :compileIt, :copyBinaries]
+task :testIt => [:unittests]
 
-task :testIt => [:runUnitTests]
+task :zipIt => [:zipPineCone]
 
-task :packageIt => [:createZip, :createNuGet]
+task :packIt => [:packPineConeNuGet]
 
-task :deployIt => [:publishNuGet]
+task :publishIt => [:publishPineConeNuGet]
 #--------------------------------------
 # Albacore tasks
 #--------------------------------------
 assemblyinfo :versionIt do |asm|
-  sharedAssemblyInfoPath = "#{@env_solutionfolderpath}SharedAssemblyInfo.cs"
-  
-  asm.input_file = sharedAssemblyInfoPath
-  asm.output_file = sharedAssemblyInfoPath
-  asm.version = @env_buildversion
-  asm.file_version = @env_buildversion  
+	sharedAssemblyInfoPath = "#{@env_solutionfolderpath}/SharedAssemblyInfo.cs"
+
+	asm.input_file = sharedAssemblyInfoPath
+	asm.output_file = sharedAssemblyInfoPath
+	asm.version = @env_buildversion
+	asm.file_version = @env_buildversion  
 end
 
-task :createCleanBuildFolders do
-  FileUtils.rm_rf(@env_buildfolderpath)
-  FileUtils.mkdir_p(@env_binariesfolderpath)
+task :ensureCleanBuildFolder do
+	FileUtils.rm_rf(@env_buildfolderpath)
+	FileUtils.mkdir_p(@env_buildfolderpath)
 end
 
-msbuild :compileIt => [:createCleanBuildFolders] do |msb|
-  msb.properties :configuration => @env_buildconfigname
-  msb.targets :Clean, :Build
-  msb.solution = "#{@env_solutionfolderpath}#{@env_solutionname}.sln"
+msbuild :buildIt => [:ensureCleanBuildFolder, :versionIt] do |msb|
+	msb.properties :configuration => @env_buildconfigname
+	msb.targets :Clean, :Build
+	msb.solution = "#{@env_solutionfolderpath}/#{@env_solutionname}.sln"
 end
 
-task :copyBinaries do
-  FileUtils.cp_r(FileList["#{@env_solutionfolderpath}Projects/#{@env_projectname}/bin/#{@env_buildconfigname}/*.*"], @env_binariesfolderpath)
+task :copyIt do
+	FileUtils.mkdir_p(pineConeOutputPath)
+	FileUtils.cp_r(FileList["#{@env_solutionfolderpath}/Projects/#{@env_projectnamePineCone}/bin/#{@env_buildconfigname}/**"], pineConeOutputPath)
 end
 
-nunit :runUnitTests do |nunit|
-  nunit.command = "#{@env_solutionfolderpath}packages/NUnit.2.5.10.11092/tools/nunit-console.exe"
-  nunit.options "/framework=v4.0.30319","/xml=#{@env_buildfolderpath}NUnit-Report-#{@env_projectname}-UnitTests.xml"
-  nunit.assemblies = FileList["#{@env_solutionfolderpath}Tests/**/#{@env_buildconfigname}/*.UnitTests.dll"].exclude(/obj\//)
+nunit :unittests do |nunit|
+	nunit.command = "#{@env_solutionfolderpath}/packages/NUnit.2.5.10.11092/tools/nunit-console.exe"
+	nunit.options "/framework=v4.0.30319","/xml=#{@env_buildfolderpath}/NUnit-Report-#{@env_solutionname}-UnitTests.xml"
+	nunit.assemblies FileList["#{@env_solutionfolderpath}/Tests/#{@env_solutionname}.**UnitTests/bin/#{@env_buildconfigname}/#{@env_solutionname}.**UnitTests.dll"]
 end
 
-zip :createZip do |zip|
-  zip.directories_to_zip "#{@env_binariesfolderpath}"
-  zip.output_file = "#{@env_buildname}.zip"
-  zip.output_path = @env_buildfolderpath
+zip :zipPineCone do |zip|
+	zip.directories_to_zip pineConeOutputPath
+	zip.output_file = "#{@env_buildname}-#{@env_projectnamePineConeAdmin}.zip"
+	zip.output_path = @env_buildfolderpath
 end
 
-exec :createNuGet do |cmd|
-  cmd.command = "NuGet.exe"
-  cmd.parameters = "pack #{@env_solutionname}.nuspec -version #{@env_buildversion} -basepath #{@env_binariesfolderpath} -outputdirectory #{@env_buildfolderpath}"
+exec :packPineConeNuGet do |cmd|
+	cmd.command = "NuGet.exe"
+	cmd.parameters = "pack #{@env_projectnamePineCone}.nuspec -version #{@env_buildversion} -basepath #{pineConeOutputPath} -outputdirectory #{@env_buildfolderpath}"
 end
 
-exec :publishNuGet do |cmd|
-  cmd.command = "NuGet.exe"
-  cmd.parameters = "push #{@env_buildfolderpath}#{@env_solutionname}.#{@env_buildversion}.nupkg #{@env_nugetPublishApiKey} -src #{@env_nugetPublishUrl}"
+exec :publishPineConeNuGet do |cmd|
+	cmd.command = "NuGet.exe"
+	cmd.parameters = "push #{@env_buildfolderpath}/#{@env_projectnamePineCone}.#{@env_buildversion}.nupkg #{@env_nugetPublishApiKey} -src #{@env_nugetPublishUrl}"
 end

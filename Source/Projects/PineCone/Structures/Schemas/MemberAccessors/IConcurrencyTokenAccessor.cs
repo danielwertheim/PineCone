04907a09658bc9e5e06f4e1ccca7a83b9304af22
Daniel Wertheim <daniel@wertheim.se>
2012-02-14 17:25:23 +0100

    - ConcurrencyToken can now be int32 or int64

--- Source/Projects/PineCone/Structures/Schemas/MemberAccessors/IConcurrencyTokenAccessor.cs
+++ Source/Projects/PineCone/Structures/Schemas/MemberAccessors/IConcurrencyTokenAccessor.cs
@@ -4,8 +4,12 @@ namespace PineCone.Structures.Schemas.MemberAccessors
 {
     public interface IConcurrencyTokenAccessor : IMemberAccessor
     {
-        Guid GetValue<T>(T item) where T : class;
+        object GetValue<T>(T item) where T : class;
 
         void SetValue<T>(T item, Guid value) where T : class;
+
+        void SetValue<T>(T item, int value) where T : class;
+
+        void SetValue<T>(T item, long value) where T : class;
     }
 }

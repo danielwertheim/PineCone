53c59552549258565c84e051b893341983d0e560
Daniel Wertheim <daniel@wertheim.se>
2012-05-19 10:36:26 +0200

    Cleaning code.

--- Source/Projects/PineCone/Structures/Schemas/IStructureProperty.cs
+++ Source/Projects/PineCone/Structures/Schemas/IStructureProperty.cs
@@ -6,29 +6,17 @@ namespace PineCone.Structures.Schemas
     public interface IStructureProperty
     {
         string Name { get; }
-
         string Path { get; }
-
         Type PropertyType { get; }
-
         IStructureProperty Parent { get; }
-
         bool IsRootMember { get; }
-
         bool IsUnique { get; }
-
         UniqueModes? UniqueMode { get; }
-
         bool IsEnumerable { get; }
-
         bool IsElement { get; }
-        
         Type ElementType { get; }
-
         bool IsReadOnly { get; }
-
         object GetValue(object item);
-
         void SetValue(object target, object value);
     }
 }
--- Source/Projects/PineCone/Structures/Schemas/MemberAccessors/IIndexAccessor.cs
+++ Source/Projects/PineCone/Structures/Schemas/MemberAccessors/IIndexAccessor.cs
@@ -7,17 +7,12 @@ namespace PineCone.Structures.Schemas.MemberAccessors
     public interface IIndexAccessor : IMemberAccessor
     {
         bool IsEnumerable { get; }
-
         bool IsElement { get; }
-
         bool IsUnique { get; }
-
         Type ElementType { get; }
-
         UniqueModes? UniqueMode { get; }
 
         IList<object> GetValues<T>(T item) where T : class;
-
         void SetValue<T>(T item, object value) where T : class;
     }
 }
